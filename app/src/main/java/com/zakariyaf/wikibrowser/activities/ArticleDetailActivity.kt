2b35093caf0f3ec7fe76830834bdc76e1843a7d5
ZakariyaF <zak.ferhi@gmail.com>
2019-08-27 10:44:07 +0100

    Create a ArticleDetailActivity

---
+++ app/src/main/java/com/zakariyaf/wikibrowser/activities/ArticleDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.zakariyaf.wikibrowser.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zakariyaf.wikibrowser.R
+
+class ArticleDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}

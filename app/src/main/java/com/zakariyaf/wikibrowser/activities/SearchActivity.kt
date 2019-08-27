db19f1c570943a896aff0b887c0642a250ba5ea1
ZakariyaF <zak.ferhi@gmail.com>
2019-08-27 11:56:29 +0100

    Update the SearchActivity with the search feature

--- app/src/main/java/com/zakariyaf/wikibrowser/activities/SearchActivity.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/activities/SearchActivity.kt
@@ -1,8 +1,13 @@
 package com.zakariyaf.wikibrowser.activities
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import com.zakariyaf.wikibrowser.R
 import kotlinx.android.synthetic.main.activity_search.*
 
@@ -21,5 +26,25 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
         return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu!!.findItem(R.id.action_search)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = searchItem!!.actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false)
+        searchView.requestFocus()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 }

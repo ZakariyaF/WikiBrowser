9e0e2f5071644f43259c16e32dccfae6980bf8f1
ZakariyaF <zak.ferhi@gmail.com>
2019-08-27 15:19:50 +0100

    Update the ExploreFragment to handle item clicks

--- app/src/main/java/com/zakariyaf/wikibrowser/fragments/ExploreFragment.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/fragments/ExploreFragment.kt
@@ -1,6 +1,7 @@
 package com.zakariyaf.wikibrowser.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.zakariyaf.wikibrowser.R
+import com.zakariyaf.wikibrowser.activities.SearchActivity
+import kotlinx.android.synthetic.main.fragment_explore.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -26,6 +25,10 @@ class ExploreFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_explore, container, false)
+        search_card_view.setOnClickListener {
+            val searchIntent = Intent(context, SearchActivity::class.java)
+            context!!.startActivity(searchIntent)
+        }
     }
 
 

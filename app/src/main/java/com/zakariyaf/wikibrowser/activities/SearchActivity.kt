9c304259b7c11168b02951e30b75c3ddeac6cab7
ZakariyaF <zak.ferhi@gmail.com>
2019-08-27 10:54:07 +0100

    Create a SearchActivity

---
+++ app/src/main/java/com/zakariyaf/wikibrowser/activities/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.zakariyaf.wikibrowser.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zakariyaf.wikibrowser.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}

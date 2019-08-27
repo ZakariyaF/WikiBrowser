3e4c2b6be29355739ceace2ef7acc48d485285a6
ZakariyaF <zak.ferhi@gmail.com>
2019-08-27 11:47:30 +0100

    Setup the ActivitySearch

--- app/src/main/java/com/zakariyaf/wikibrowser/activities/SearchActivity.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/activities/SearchActivity.kt
@@ -2,12 +2,24 @@ package com.zakariyaf.wikibrowser.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.zakariyaf.wikibrowser.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item!!.itemId == android.R.id.home) {
+            finish()
+        }
+        return true
     }
 }

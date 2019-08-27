13efe1888435600fcab20d44a28853adefb06119
ZakariyaF <zak.ferhi@gmail.com>
2019-08-27 11:04:35 +0100

    Update the ArticleDetailActivity

--- app/src/main/java/com/zakariyaf/wikibrowser/activities/ArticleDetailActivity.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/activities/ArticleDetailActivity.kt
@@ -2,12 +2,24 @@ package com.zakariyaf.wikibrowser.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.zakariyaf.wikibrowser.R
+import kotlinx.android.synthetic.main.activity_article_detail.*
 
 class ArticleDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_detail)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item!!.itemId == android.R.id.home) {
+            finish()
+        }
+        return true
     }
 }

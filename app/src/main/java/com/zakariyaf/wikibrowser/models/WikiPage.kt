3c2b7a4e2f3c4ec20662429cd5b4ce78805b0474
ZakariyaF <zak.ferhi@gmail.com>
2019-08-28 11:17:17 +0100

    Setup the WikiPage model

--- app/src/main/java/com/zakariyaf/wikibrowser/models/WikiPage.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/models/WikiPage.kt
@@ -1,4 +1,8 @@
 package com.zakariyaf.wikibrowser.models
 
 class WikiPage {
+    var pageId: Int? = null
+    var title: String? = null
+    var fullUrl: String? = null
+    var thumbnail: WikiThumbnail? = null
 }

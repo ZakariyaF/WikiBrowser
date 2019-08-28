7de1117e4d5fd9174e9847e4062212a2ff15ddc5
ZakariyaF <zak.ferhi@gmail.com>
2019-08-28 14:53:38 +0100

    Update the ArticleDetailActivity to use data from the model

--- app/src/main/java/com/zakariyaf/wikibrowser/activities/ArticleDetailActivity.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/activities/ArticleDetailActivity.kt
@@ -3,10 +3,17 @@ package com.zakariyaf.wikibrowser.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.google.gson.Gson
 import com.zakariyaf.wikibrowser.R
+import com.zakariyaf.wikibrowser.models.WikiPage
 import kotlinx.android.synthetic.main.activity_article_detail.*
 
 class ArticleDetailActivity : AppCompatActivity() {
+
+    private var currentPage: WikiPage? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +21,17 @@ class ArticleDetailActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        val wikiPageJson = intent.getStringExtra("page")
+        currentPage = Gson().fromJson<WikiPage>(wikiPageJson, WikiPage::class.java)
+
+        article_detail_webview?.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return true
+            }
+        }
+
+        article_detail_webview.loadUrl(currentPage!!.fullUrl)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {

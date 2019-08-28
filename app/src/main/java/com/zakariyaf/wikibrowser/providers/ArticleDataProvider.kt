315cca6431d1411aa43b5ecd970810994ae7fcef
ZakariyaF <zak.ferhi@gmail.com>
2019-08-28 12:45:47 +0100

    Setup the ArticleDataProvider

--- app/src/main/java/com/zakariyaf/wikibrowser/providers/ArticleDataProvider.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/providers/ArticleDataProvider.kt
@@ -1,4 +1,42 @@
 package com.zakariyaf.wikibrowser.providers
 
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.github.kittinunf.fuel.httpGet
+import com.google.gson.Gson
+import com.zakariyaf.wikibrowser.models.Urls
+import com.zakariyaf.wikibrowser.models.WikiResult
+import java.io.Reader
+
 class ArticleDataProvider {
+    init {
+        FuelManager.instance.baseHeaders = mapOf("User-Agent" to "Zakariyaf Wikipedia")
+    }
+
+    fun search(term: String, skip: Int, take: Int, respondeHandler: (result: WikiResult) -> Unit?) {
+        Urls.getSearchUrl(term, skip, take).httpGet()
+            .responseObject(WikipediaDataDeserializer()) { _, response, result ->
+                if (response.statusCode != 200) {
+                    throw Exception("Unable to get articles!")
+                }
+                val (data, _) = result
+                respondeHandler.invoke(data as WikiResult)
+            }
+    }
+
+    fun getRandom(take: Int, respondeHandler: (result: WikiResult) -> Unit?) {
+        Urls.getRandomUrl(take).httpGet()
+            .responseObject(WikipediaDataDeserializer()) { _, response, result ->
+                if (response.statusCode != 200) {
+                    throw Exception("Unable to get articles!")
+                }
+                val (data, _) = result
+                respondeHandler.invoke(data as WikiResult)
+            }
+    }
+
+    class WikipediaDataDeserializer : ResponseDeserializable<WikiResult> {
+        override fun deserialize(reader: Reader) = Gson().fromJson(reader, WikiResult::class.java)
+    }
+
 }

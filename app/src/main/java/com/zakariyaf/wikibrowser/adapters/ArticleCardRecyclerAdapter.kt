5a64dc7f7c0ab4b4a16453b9cc63e8452149a189
ZakariyaF <zak.ferhi@gmail.com>
2019-08-28 14:03:46 +0100

    Update the card adapter to use actual page data

--- app/src/main/java/com/zakariyaf/wikibrowser/adapters/ArticleCardRecyclerAdapter.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/adapters/ArticleCardRecyclerAdapter.kt
@@ -5,18 +5,23 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.zakariyaf.wikibrowser.R
 import com.zakariyaf.wikibrowser.holders.CardHolder
+import com.zakariyaf.wikibrowser.models.WikiPage
 
 class ArticleCardRecyclerAdapter() : RecyclerView.Adapter<CardHolder>() {
+
+    val currentResults: ArrayList<WikiPage> = ArrayList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardHolder {
         var cardItem = LayoutInflater.from(parent?.context).inflate(R.layout.article_card_item, parent, false)
         return CardHolder(cardItem)
     }
 
     override fun getItemCount(): Int {
-        return 10 //TODO change this
+        return currentResults.size
     }
 
     override fun onBindViewHolder(holder: CardHolder, position: Int) {
-
+        var page = currentResults[position]
+        holder.updateWithPage(page)
     }
 }
--- app/src/main/java/com/zakariyaf/wikibrowser/holders/CardHolder.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/holders/CardHolder.kt
@@ -5,8 +5,17 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.zakariyaf.wikibrowser.R
+import com.zakariyaf.wikibrowser.models.WikiPage
 
 class CardHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val articleImageView: ImageView = itemView.findViewById(R.id.article_image)
     private val titleTextView: TextView = itemView.findViewById(R.id.article_title)
+
+    private var currentPage: WikiPage? = null
+
+    fun updateWithPage(page: WikiPage) {
+        currentPage = page
+        titleTextView.text = page.title
+
+    }
 }

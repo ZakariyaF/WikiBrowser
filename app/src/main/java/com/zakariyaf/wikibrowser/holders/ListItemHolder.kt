8b28710649e9f9767364a3e6d2f7871c4dcc741a
ZakariyaF <zak.ferhi@gmail.com>
2019-08-28 14:58:14 +0100

    Handle list item click event

--- app/src/main/java/com/zakariyaf/wikibrowser/holders/ListItemHolder.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/holders/ListItemHolder.kt
@@ -1,11 +1,14 @@
 package com.zakariyaf.wikibrowser.holders
 
+import android.content.Intent
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import com.zakariyaf.wikibrowser.R
+import com.zakariyaf.wikibrowser.activities.ArticleDetailActivity
 import com.zakariyaf.wikibrowser.models.WikiPage
 
 class ListItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -13,6 +16,15 @@ class ListItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val titleTextView: TextView = itemView.findViewById(R.id.result_title)
 
     private var currentPage: WikiPage? = null
+
+    init {
+        itemView.setOnClickListener { view: View? ->
+            var detailPageIntent = Intent(itemView.context, ArticleDetailActivity::class.java)
+            var pageJson = Gson().toJson(currentPage)
+            detailPageIntent.putExtra("page", pageJson)
+            itemView.context.startActivity(detailPageIntent)
+        }
+    }
 
     fun updateWithPage(page: WikiPage) {
         if (page.thumbnail != null)

ae983c703623489ccf1417119045e7198d41575b
ZakariyaF <zak.ferhi@gmail.com>
2019-09-02 18:16:39 +0100

    Update the FavoritesFragment with data from the wikiManager

--- app/src/main/java/com/zakariyaf/wikibrowser/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/fragments/FavoritesFragment.kt
@@ -15,7 +15,9 @@ import com.zakariyaf.wikibrowser.WikiApplication
 import com.zakariyaf.wikibrowser.adapters.ArticleCardRecyclerAdapter
 import com.zakariyaf.wikibrowser.adapters.ArticleListItemRecyclerAdapter
 import com.zakariyaf.wikibrowser.managers.WikiManager
+import com.zakariyaf.wikibrowser.models.WikiPage
 import kotlinx.android.synthetic.main.fragment_favorites.*
+import org.jetbrains.anko.doAsync
 
 /**
  * A simple [Fragment] subclass.
@@ -25,6 +27,7 @@ class FavoritesFragment : Fragment() {
 
     private var wikiManager: WikiManager? = null
     var favoritesRecycler: RecyclerView? = null
+    private val adapter: ArticleCardRecyclerAdapter = ArticleCardRecyclerAdapter()
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -39,10 +42,20 @@ class FavoritesFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_favorites, container, false)
         favoritesRecycler = view.findViewById(R.id.favorites_article_recycler)
         favoritesRecycler!!.layoutManager = LinearLayoutManager(context)
-        favoritesRecycler!!.adapter = ArticleCardRecyclerAdapter()
+        favoritesRecycler!!.adapter = adapter
 
         return view
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        doAsync {
+            val favoriteArticles = wikiManager!!.getFavorites()
+            adapter.currentResults.clear()
+            adapter.currentResults.addAll(favoriteArticles as ArrayList<WikiPage>)
+            activity?.runOnUiThread { adapter.notifyDataSetChanged() }
+        }
+    }
 
 }

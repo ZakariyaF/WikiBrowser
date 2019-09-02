a7b39ccd6fa75d2e3ab4d0d3b47d9e69f1dd477c
ZakariyaF <zak.ferhi@gmail.com>
2019-09-02 12:13:31 +0100

    Setup the HistoryRepository class

--- app/src/main/java/com/zakariyaf/wikibrowser/repositories/HistoryRepository.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/repositories/HistoryRepository.kt
@@ -1,4 +1,46 @@
 package com.zakariyaf.wikibrowser.repositories
 
-class HistoryRepository {
+import com.google.gson.Gson
+import com.zakariyaf.wikibrowser.models.WikiPage
+import com.zakariyaf.wikibrowser.models.WikiThumbnail
+import org.jetbrains.anko.db.delete
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.rowParser
+
+class HistoryRepository(val databaseHelper: ArticleDBOpenHelper) {
+    private val TABLE_NAME: String = "History"
+
+    fun addHistory(page: WikiPage) {
+        databaseHelper.use {
+            insert(
+                TABLE_NAME,
+                "id" to page.pageId,
+                "title" to page.title,
+                "url" to page.fullurl,
+                "thumbnailJson" to Gson().toJson(page.thumbnail)
+            )
+        }
+    }
+
+    fun removeHistoryPageById(pageId: Int) {
+        databaseHelper.use {
+            delete(
+                TABLE_NAME,
+                "id = {pageId}", "pageId" to pageId
+            )
+        }
+    }
+
+    fun getAllHistory(): ArrayList<WikiPage> {
+        var pages = ArrayList<WikiPage>()
+        val articleRowParser = rowParser { id: Int, title: String, url: String, thumbnailJson: String ->
+            val page = WikiPage()
+            page.title = title
+            page.pageId = id
+            page.fullurl = url
+            page.thumbnail = Gson().fromJson(thumbnailJson, WikiThumbnail::class.java)
+            pages.add(page)
+        }
+        return pages
+    }
 }

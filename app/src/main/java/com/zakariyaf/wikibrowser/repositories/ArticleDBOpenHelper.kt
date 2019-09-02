e05160fde97c13965c3ce06ff8342adda4be1f59
ZakariyaF <zak.ferhi@gmail.com>
2019-09-02 11:48:32 +0100

    Setup the ArticleDBOpenHelper class

--- app/src/main/java/com/zakariyaf/wikibrowser/repositories/ArticleDBOpenHelper.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/repositories/ArticleDBOpenHelper.kt
@@ -1,4 +1,30 @@
 package com.zakariyaf.wikibrowser.repositories
 
-class ArticleDBOpenHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+class ArticleDBOpenHelper(context: Context) :
+    ManagedSQLiteOpenHelper(context, "ArticlesDatabase.db", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.createTable(
+            "Favorites", true,
+            "id" to INTEGER + PRIMARY_KEY,
+            "title" to TEXT,
+            "url" to TEXT,
+            "thumbnailJson" to TEXT
+        )
+        db?.createTable(
+            "History", true,
+            "id" to INTEGER + PRIMARY_KEY,
+            "title" to TEXT,
+            "url" to TEXT,
+            "thumbnailJson" to TEXT
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
 }

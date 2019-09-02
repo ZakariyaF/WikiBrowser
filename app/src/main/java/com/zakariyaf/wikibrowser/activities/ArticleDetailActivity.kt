d44bc63f5aa0d042b2732e3df29c3e4b7fce6a01
ZakariyaF <zak.ferhi@gmail.com>
2019-09-02 18:09:51 +0100

    Update the ArticleDetailActivity with the favorite menu

--- app/src/main/java/com/zakariyaf/wikibrowser/activities/ArticleDetailActivity.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/activities/ArticleDetailActivity.kt
@@ -2,6 +2,7 @@ package com.zakariyaf.wikibrowser.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
@@ -12,6 +13,7 @@ import com.zakariyaf.wikibrowser.WikiApplication
 import com.zakariyaf.wikibrowser.managers.WikiManager
 import com.zakariyaf.wikibrowser.models.WikiPage
 import kotlinx.android.synthetic.main.activity_article_detail.*
+import org.jetbrains.anko.toast
 
 class ArticleDetailActivity : AppCompatActivity() {
 
@@ -30,6 +32,8 @@ class ArticleDetailActivity : AppCompatActivity() {
         val wikiPageJson = intent.getStringExtra("page")
         currentPage = Gson().fromJson<WikiPage>(wikiPageJson, WikiPage::class.java)
 
+        supportActionBar?.title = currentPage?.title
+
         article_detail_webview?.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                 return true
@@ -40,9 +44,25 @@ class ArticleDetailActivity : AppCompatActivity() {
         wikiManager?.addHistory(currentPage!!)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.article_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item!!.itemId == android.R.id.home) {
             finish()
+        } else if (item.itemId == R.id.action_favorite) {
+            try {
+                if (wikiManager!!.getIsFavorite(currentPage!!.pageId!!)) {
+                    wikiManager!!.removeFavorite(currentPage!!.pageId!!)
+                    toast("Article removed from favorites")
+                } else {
+                    wikiManager!!.addFavorite(currentPage!!)
+                    toast("Article added to favorites")
+                }
+            } catch (ex: Exception) {
+                toast("Unable to update this article!")
+            }
         }
         return true
     }

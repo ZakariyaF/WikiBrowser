d8a14b9677aa0108e15d3c488e2809cd547a8c9d
ZakariyaF <zak.ferhi@gmail.com>
2019-09-02 12:29:22 +0100

    Setup the WikiManager class

--- app/src/main/java/com/zakariyaf/wikibrowser/managers/WikiManager.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/managers/WikiManager.kt
@@ -1,4 +1,63 @@
 package com.zakariyaf.wikibrowser.managers
 
-class WikiManager {
+import com.zakariyaf.wikibrowser.models.WikiPage
+import com.zakariyaf.wikibrowser.models.WikiResult
+import com.zakariyaf.wikibrowser.providers.ArticleDataProvider
+import com.zakariyaf.wikibrowser.repositories.FavoritesRepository
+import com.zakariyaf.wikibrowser.repositories.HistoryRepository
+
+class WikiManager(
+    private val provider: ArticleDataProvider,
+    private val favoritesRepository: FavoritesRepository,
+    private val historyRepository: HistoryRepository
+) {
+
+    private var favoritesCache: ArrayList<WikiPage>? = null
+    private var historyCache: ArrayList<WikiPage>? = null
+
+    fun search(term: String, skip: Int, take: Int, handler: (result: WikiResult) -> Unit?) {
+        return provider.search(term, skip, take, handler)
+    }
+
+    fun getRandom(take: Int, handler: (result: WikiResult) -> Unit?) {
+        return provider.getRandom(take, handler)
+    }
+
+    fun getHistory(): ArrayList<WikiPage>? {
+        if (historyCache == null)
+            historyCache = historyRepository.getAllHistory()
+        return historyCache
+    }
+
+    fun getFavorites(): ArrayList<WikiPage>? {
+        if (favoritesCache == null)
+            favoritesCache = favoritesRepository.getAllFavorites()
+        return favoritesCache
+    }
+
+    fun addFavorite(page: WikiPage) {
+        favoritesCache?.add(page)
+        favoritesRepository.addFavorite(page)
+    }
+
+    fun removeFavorite(pageId: Int) {
+        favoritesRepository.removeFavoriteById(pageId)
+        favoritesCache = favoritesCache!!.filter { it.pageId != pageId } as ArrayList<WikiPage>
+    }
+
+    fun getIsFavorite(pageId: Int): Boolean {
+        return favoritesRepository.isArticleFavorite(pageId)
+    }
+
+    fun addHistory(page: WikiPage) {
+        historyCache?.add(page)
+        historyRepository.addHistory(page)
+    }
+
+    fun clearHistory() {
+        historyCache?.clear()
+        val allHistory = historyRepository.getAllHistory()
+        allHistory?.forEach { historyRepository.removeHistoryPageById(it.pageId!!) }
+    }
+
 }

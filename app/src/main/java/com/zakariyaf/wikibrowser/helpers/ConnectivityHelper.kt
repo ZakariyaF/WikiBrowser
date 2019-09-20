846a7a48d70d630c8f9ee714690fec697bf1f2d6
ZakariyaF <zak.ferhi@gmail.com>
2019-09-20 14:42:28 +0100

    Setup the ConnectivityHelper

--- app/src/main/java/com/zakariyaf/wikibrowser/helpers/ConnectivityHelper.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/helpers/ConnectivityHelper.kt
@@ -1,4 +1,31 @@
 package com.zakariyaf.wikibrowser.helpers
 
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+
 object ConnectivityHelper {
+    fun hasInternetConnection(): Single<Boolean> {
+        return Single.fromCallable {
+            try {
+                // Connect to Google DNS to check for connection
+                val timeoutMs = 1500
+                val socket = Socket()
+                val socketAddress = InetSocketAddress("8.8.8.8", 53)
+
+                socket.connect(socketAddress, timeoutMs)
+                socket.close()
+
+                true
+            } catch (e: IOException) {
+                false
+            }
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
 }

8c3c63417708cddd3b9269fa4fc689314ce92906
umairayub79 <umairayub79@gmail.com>
2019-10-03 11:20:12 +0500

    Created No History and No Favorites TextView

--- app/src/main/java/com/zakariyaf/wikibrowser/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/zakariyaf/wikibrowser/fragments/FavoritesFragment.kt
@@ -3,21 +3,18 @@ package com.zakariyaf.wikibrowser.fragments
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-
 import com.zakariyaf.wikibrowser.R
 import com.zakariyaf.wikibrowser.WikiApplication
 import com.zakariyaf.wikibrowser.adapters.ArticleCardRecyclerAdapter
-import com.zakariyaf.wikibrowser.adapters.ArticleListItemRecyclerAdapter
 import com.zakariyaf.wikibrowser.managers.WikiManager
 import com.zakariyaf.wikibrowser.models.WikiPage
-import kotlinx.android.synthetic.main.fragment_favorites.*
 import org.jetbrains.anko.doAsync
 
 /**
@@ -28,6 +25,8 @@ class FavoritesFragment : Fragment() {
 
     private var wikiManager: WikiManager? = null
     var favoritesRecycler: RecyclerView? = null
+    var noFav_textview: TextView? = null
+
     private val adapter: ArticleCardRecyclerAdapter = ArticleCardRecyclerAdapter()
 
     override fun onAttach(context: Context?) {
@@ -35,6 +34,7 @@ class FavoritesFragment : Fragment() {
 
         wikiManager = (activity?.applicationContext as WikiApplication).wikiManager
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,7 +42,9 @@ class FavoritesFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_favorites, container, false)
         favoritesRecycler = view.findViewById(R.id.favorites_article_recycler)
-        favoritesRecycler!!.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        noFav_textview = view.findViewById(R.id.noFavTextView)
+        favoritesRecycler!!.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         favoritesRecycler!!.adapter = adapter
 
         return view
@@ -53,9 +55,15 @@ class FavoritesFragment : Fragment() {
 
         doAsync {
             val favoriteArticles = wikiManager!!.getFavorites()
-            adapter.currentResults.clear()
-            adapter.currentResults.addAll(favoriteArticles as ArrayList<WikiPage>)
-            activity?.runOnUiThread { adapter.notifyDataSetChanged() }
+            if (favoriteArticles!!.isEmpty()) {
+                favoritesRecycler?.visibility = View.INVISIBLE
+                noFav_textview?.visibility = View.VISIBLE
+            } else {
+                favoritesRecycler?.visibility = View.VISIBLE
+                adapter.currentResults.clear()
+                adapter.currentResults.addAll(favoriteArticles as ArrayList<WikiPage>)
+                activity?.runOnUiThread { adapter.notifyDataSetChanged() }
+            }
         }
     }
 
